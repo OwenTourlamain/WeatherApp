5fdc61ce77f4889f4bffaf52a81ade3c547e9ae3
Owen Tourlamain <owen.tourlamain@gmail.com>
2018-12-16 03:53:42 +0000

    building the db structure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import yaml
 import gi
-import weather
+from weather import Weather
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk
 
@@ -39,25 +39,25 @@ class WeatherAppWindow (Gtk.Window):
 with open("conf.yaml") as conf_file:
     conf = yaml.load(conf_file, Loader=yaml.Loader)
 
-weather = weather(conf)
+weather = Weather(conf)
 
 # Have a pro subscription? Then use:
 # owm = pyowm.OWM(API_key='your-API-key', subscription_type='pro')
 
 # Search for current weather in London (Great Britain)
-observation = owm.weather_at_place('London,GB')
-w = observation.get_weather()
-print(w)                      # <Weather - reference time=2013-12-18 09:20,
+#observation = owm.weather_at_place('London,GB')
+#w = observation.get_weather()
+#print(w)                      # <Weather - reference time=2013-12-18 09:20,
                               # status=Clouds>
 
 # Weather details
-w.get_wind()                  # {'speed': 4.6, 'deg': 330}
-w.get_humidity()              # 87
-w.get_temperature('celsius')  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
+#w.get_wind()                  # {'speed': 4.6, 'deg': 330}
+#w.get_humidity()              # 87
+#w.get_temperature('celsius')  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
 
 # Search current weather observations in the surroundings of
 # lat=22.57W, lon=43.12S (Rio de Janeiro, BR)
-observation_list = owm.weather_around_coords(-22.57, -43.12)
+#observation_list = owm.weather_around_coords(-22.57, -43.12)
 
 
 window = WeatherAppWindow()
--- weather.py
+++ weather.py
@@ -1,11 +1,40 @@
 import sqlite3
 import pyowm
+from jsonstreamer import JSONStreamer
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk, Gdk
 
 
 class Weather:
 
-    db = sqlite3.connect('weather.db')
-    print (db)
-
     def __init__(self, conf):
-        self.owm = pyowm.OWM(conf["api_key"])  # You MUST provide a valid API key
+
+        loc_table = "Locations"
+        loc_cols = ["id", "name", "country", "lat", "lon"]
+        day_table = "Days"
+        loc_create = "create table if not exists {0} ({1}, {2}, {3}, {4}, {5})"
+
+        self.owm = pyowm.OWM(conf["api_key"])  # you MUST provide a valid API key
+
+        self.db = sqlite3.connect('weather.db')
+        cursor = self.db.cursor()
+
+        cursor.execute(loc_create.format(loc_table, *loc_cols))
+
+        cursor.execute("Select count(*) from %s" % loc_table)
+        if cursor.fetchone()[0] == 0:
+            self.populate_db()
+
+    def populate_db(self):
+        print("opening json")
+        streamer = JSONStreamer()
+        i = 0
+        with open("city.list.json") as file:
+            for line in file:
+                # streamer.add_catch_all_listener(_catch_all)
+                streamer.consume(line)
+                i += 1
+            streamer.close()
+        print(i)
+        print(streamer)
